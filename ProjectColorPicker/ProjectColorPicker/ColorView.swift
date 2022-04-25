7ebf5257faabf64118aa0da47af44a74543c4066
Marilise Morona <marilise@192.168.1.4>
2022-04-25 20:27:39 -0300

    feat: action created

--- ProjectColorPicker/ProjectColorPicker/ColorView.swift
+++ ProjectColorPicker/ProjectColorPicker/ColorView.swift
@@ -9,12 +9,38 @@ import UIKit
 
 class ColorView: UIView {
 
+    lazy var stackColorView: UIStackView = {
+        let stackColorView = UIStackView(arrangedSubviews: [coloredRectangle, firstSwitch])
+        stackColorView.translatesAutoresizingMaskIntoConstraints = false
+        stackColorView.axis = .vertical
+        stackColorView.spacing = 30
+        stackColorView.backgroundColor = .orange
+        return stackColorView
+    }()
+    
     lazy var coloredRectangle : UIView = {
         let colorView = UIView()
-        colorView.backgroundColor = .black
         colorView.translatesAutoresizingMaskIntoConstraints = false
+        colorView.backgroundColor = .black
         return colorView
     }()
+    
+    lazy var firstSwitch: UISwitch = {
+       let firstSwitch = UISwitch()
+        firstSwitch.translatesAutoresizingMaskIntoConstraints = false
+        firstSwitch.addTarget(self, action: #selector(changedColorView(mySwitch:)), for: UIControl.Event.valueChanged)
+        return firstSwitch
+    }()
+    
+//    lazy var stackSwitchView: UIStackView = {
+//        let stackSwitchView = UIStackView()
+//        stackSwitchView.translatesAutoresizingMaskIntoConstraints = false
+//        stackSwitchView.axis = .vertical
+//        stackSwitchView.spacing = 30
+//        stackSwitchView.backgroundColor = .brown
+//        return stackSwitchView
+//    }()
+    
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -26,7 +52,7 @@ class ColorView: UIView {
     }
     
     private func setUp(){
-        addSubview(coloredRectangle)
+        addSubview(stackColorView)
         constraintLayout()
         
     }
@@ -35,11 +61,19 @@ class ColorView: UIView {
         translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
-            coloredRectangle.topAnchor.constraint(equalTo: topAnchor, constant: 100),
-            coloredRectangle.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -30),
-            coloredRectangle.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 30),
-            coloredRectangle.heightAnchor.constraint(equalToConstant: 300)
+            stackColorView.topAnchor.constraint(equalTo: topAnchor, constant: 100),
+            stackColorView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -30),
+            stackColorView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 30),
+            stackColorView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -30)
         ])
+    }
+    
+    @objc func changedColorView(mySwitch: UISwitch){
+        if mySwitch.isOn{
+            coloredRectangle.backgroundColor = .systemPink
+        } else {
+            coloredRectangle.backgroundColor = .gray
+        }
     }
 
 }
