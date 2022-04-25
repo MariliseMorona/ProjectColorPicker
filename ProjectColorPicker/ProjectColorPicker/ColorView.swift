32fc0c204e9a875dcd96c08b67070e475ff50dff
Marilise Morona <marilise@192.168.1.4>
2022-04-25 19:50:11 -0300

    feat: outputs created

--- ProjectColorPicker/ProjectColorPicker/ColorView.swift
+++ ProjectColorPicker/ProjectColorPicker/ColorView.swift
@@ -12,7 +12,34 @@ class ColorView: UIView {
     lazy var coloredRectangle : UIView = {
         let colorView = UIView()
         colorView.backgroundColor = .black
+        colorView.translatesAutoresizingMaskIntoConstraints = false
         return colorView
     }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUp()
+        
+    }
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setUp(){
+        addSubview(coloredRectangle)
+        constraintLayout()
+        
+    }
+    
+    private func constraintLayout(){
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            coloredRectangle.topAnchor.constraint(equalTo: topAnchor, constant: 100),
+            coloredRectangle.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -30),
+            coloredRectangle.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 30),
+            coloredRectangle.heightAnchor.constraint(equalToConstant: 300)
+        ])
+    }
 
 }
--- ProjectColorPicker/ProjectColorPicker/ViewController.swift
+++ ProjectColorPicker/ProjectColorPicker/ViewController.swift
@@ -14,9 +14,26 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .yellow
+        setUp()
         
     }
-
-
+    
+    private func setUp(){
+        view.addSubview(colorView)
+        constraintLayout()
+       
+    }
+    
+    private func constraintLayout(){
+        colorView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            colorView.topAnchor.constraint(equalTo: view.topAnchor),
+            colorView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            colorView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            colorView.leadingAnchor.constraint(equalTo: view.leadingAnchor)
+            ])
+        
+    }
 }
 
